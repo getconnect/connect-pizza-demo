9ebc11c16984c80515bfba89bfbfd9f3a679b49b
Chad Edrupt <chad@tipihq.com>
2015-07-09 11:49:07 +1000

    refactors Order into a single place

---
+++ src/js/Ordering/Order.js
@@ -0,0 +1,87 @@
+
+class Order {
+
+	constructor() {
+		this.pizzaOptions = this.getPizzaOptions();
+		this.selectedPizzaOptions = {
+    		type: 0,
+    		size: 0,
+    		base: 0,
+    		sauce: 0,
+    		delivery: 0
+    	};
+
+    	this.deliveryOptions = this.getDeliveryOptions();
+    	this.selectedDeliveryOption = 0;
+
+    	this.quantity = 1;
+	}
+
+	calculateTotal() {
+		var total = Object.keys(this.pizzaOptions).reduce((total, option) => {
+			var selectedIndex = this.selectedPizzaOptions[option];
+			var selectedOption = this.pizzaOptions[option].options[selectedIndex];
+			return total + selectedOption.price;
+		}, 0);
+		total *= Math.max(this.quantity, 1);
+		total += this.deliveryOptions.options[this.selectedDeliveryOption].price;
+		return total;
+	}
+
+	getPizzaOptions() {
+		return {
+			type: {
+				title: 'Choose a pizza',
+				isExtra: false,
+				options: [
+					{ title: 'Supreme', price: 10, imageUrl: 'images/supreme.png'},
+					{ title: 'Cheese', price: 8, imageUrl: 'images/cheese.png'},
+					{ title: 'Hawaiian', price: 10, imageUrl: 'images/hawaiian.png' },
+					{ title: 'Pepperoni', price: 10, imageUrl: 'images/pepperoni.png' },
+					{ title: 'Mexicana', price: 10, imageUrl: 'images/mexicana.png' },
+					{ title: 'Vege', price: 10, imageUrl: 'images/vege.png' }
+				]
+			},
+			size: {
+				title: 'Choose a size',
+				isExtra: true,
+				options: [
+					{ title: 'Regular', price: 0, imageUrl: 'images/regular.png' },
+					{ title: 'Large', price: 4, imageUrl: 'images/large.png' },
+					{ title: 'Extra Large', price: 7, imageUrl: 'images/extra-large.png' }
+				]
+			},
+			base: {
+				title: 'Choose a base',
+				isExtra: true,
+				options: [
+					{ title: 'Regular Crust', price: 0, imageUrl: null },
+					{ title: 'Cheesy Crust', price: 3, imageUrl: null },
+					{ title: 'Gluten Free', price: 4, imageUrl: null }
+				]
+			},
+			sauce: {
+				title: 'Choose a sauce',
+				isExtra: true,
+				options: [
+					{ title: 'Tomato', price: 0, imageUrl: null },
+					{ title: 'Barbeque', price: 0, imageUrl: null },
+					{ title: 'Spicy Habanero', price: 0, imageUrl: null }
+				]
+			}
+		};
+	}
+
+	getDeliveryOptions() {
+		return {
+			title: 'Pickup or delivery?',
+			isExtra: true,
+			options: [
+				{ title: 'Pickup', price: 0, imageUrl: 'images/pickup.png' },
+				{ title: 'Delivery', price: 4, imageUrl: 'images/delivery.png' }
+			]
+		};
+	}
+}
+
+export default Order;
--- src/js/Ordering/OrderForm.jsx
+++ src/js/Ordering/OrderForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'rebass';
-import { pizzaOptions, deliveryOptions } from './Defaults.js';
+import Order from './Order.js';
 import HeaderLogo from './HeaderLogo.jsx';
 import OrderConfirmation from './OrderConfirmation.jsx';
 import OptionPicker from './OptionPicker.jsx';
@@ -9,93 +9,79 @@ import TotalsSection from './TotalsSection.jsx';
 
 class OrderForm extends React.Component {
 
-	constructor(props) {
-    	super(props);
-    	this.state = this.initialState();
+    constructor(props) {
+        super(props);
+        this.state = this.initialState();
 
-    	this.onPizzaSelected = this.onItemSelected.bind(this, 'type');
-    	this.onSizeSelected = this.onItemSelected.bind(this, 'size');
-    	this.onBaseSelected = this.onItemSelected.bind(this, 'base');
-    	this.onSauceSelected = this.onItemSelected.bind(this, 'sauce');
-  	}
+        this.onPizzaSelected = this.onItemSelected.bind(this, 'type');
+        this.onSizeSelected = this.onItemSelected.bind(this, 'size');
+        this.onBaseSelected = this.onItemSelected.bind(this, 'base');
+        this.onSauceSelected = this.onItemSelected.bind(this, 'sauce');
+    }
 
-	render() {
-		var { pizzaOptions, deliveryOptions } = this.props;
-		var { selectedPizzaOptions, selectedDeliveryOption, quantity, orderPlaced } = this.state;
+    render() {
+        var { order, orderPlaced } = this.state;
+        var { pizzaOptions, 
+              selectedPizzaOptions, 
+              deliveryOptions, 
+              selectedDeliveryOption, 
+              quantity } = order;
 
-		if (orderPlaced) {
-			return <OrderConfirmation onOrderAgain={this.onOrderAgain.bind(this)} quantity={quantity} />
-		}
+        if (orderPlaced) {
+            return <OrderConfirmation onOrderAgain={this.onOrderAgain.bind(this)} quantity={quantity} />
+        }
 
-		var total = Object.keys(pizzaOptions).reduce((total, option) => {
-			var selectedIndex = selectedPizzaOptions[option];
-			var selectedOption = pizzaOptions[option].options[selectedIndex];
-			return total + selectedOption.price;
-		}, 0);
-		total *= Math.max(quantity, 1);
-		total += deliveryOptions.options[selectedDeliveryOption].price;
+        var total = order.calculateTotal();
 
-		return (
-			<Container>
-				<HeaderLogo />
-				<OptionPicker {...pizzaOptions.type} selected={selectedPizzaOptions.type} onItemSelected={this.onPizzaSelected} />
-				<OptionPicker {...pizzaOptions.size} selected={selectedPizzaOptions.size} onItemSelected={this.onSizeSelected} />
-				<OptionPicker {...pizzaOptions.base} selected={selectedPizzaOptions.base} onItemSelected={this.onBaseSelected} />
-				<OptionPicker {...pizzaOptions.sauce} selected={selectedPizzaOptions.sauce} onItemSelected={this.onSauceSelected} />
-				<QuantityPicker quantity={quantity} onQuantityUpdated={this.onQuantityUpdated.bind(this)} />
-				<OptionPicker {...deliveryOptions} selected={selectedDeliveryOption} onItemSelected={this.onDeliverySelected.bind(this)} />
-				<TotalsSection total={total} onPlaceOrder={this.onPlaceOrder.bind(this)} />
-			</Container>
-		);
-	}
+        return (
+            <Container>
+                <HeaderLogo />
+                <OptionPicker {...pizzaOptions.type} selected={selectedPizzaOptions.type} onItemSelected={this.onPizzaSelected} />
+                <OptionPicker {...pizzaOptions.size} selected={selectedPizzaOptions.size} onItemSelected={this.onSizeSelected} />
+                <OptionPicker {...pizzaOptions.base} selected={selectedPizzaOptions.base} onItemSelected={this.onBaseSelected} />
+                <OptionPicker {...pizzaOptions.sauce} selected={selectedPizzaOptions.sauce} onItemSelected={this.onSauceSelected} />
+                <QuantityPicker quantity={quantity} onQuantityUpdated={this.onQuantityUpdated.bind(this)} />
+                <OptionPicker {...deliveryOptions} selected={selectedDeliveryOption} onItemSelected={this.onDeliverySelected.bind(this)} />
+                <TotalsSection total={total} onPlaceOrder={this.onPlaceOrder.bind(this)} />
+            </Container>
+        );
+    }
 
-	initialState() {
-		return { 
-    		selectedPizzaOptions: {
-    			type: 0,
-    			size: 0,
-    			base: 0,
-    			sauce: 0,
-    			delivery: 0
-    		},
-    		selectedDeliveryOption: 0,
-    		quantity: 1,
-    		orderPlaced: false
-    	};
-	}
+    initialState() {
+        return  {
+            order: new Order(),
+            orderPlaced: false
+        }
+    }
 
-	onItemSelected(sectionName, selectedIndex) {
-		var state = this.state;
-		state.selectedPizzaOptions[sectionName] = selectedIndex;
-		this.setState(state);
-	}
+    onItemSelected(sectionName, selectedIndex) {
+        var state = this.state;
+        state.order.selectedPizzaOptions[sectionName] = selectedIndex;
+        this.setState(state);
+    }
 
-	onDeliverySelected(selectedIndex) {
-		var state = this.state;
-		state.selectedDeliveryOption = selectedIndex;
-		this.setState(state);
-	}
+    onDeliverySelected(selectedIndex) {
+        var state = this.state;
+        state.order.selectedDeliveryOption = selectedIndex;
+        this.setState(state);
+    }
 
-	onQuantityUpdated(quantity) {
-		var state = this.state;
-		state.quantity = quantity;
-		this.setState(state);
-	}
+    onQuantityUpdated(quantity) {
+        var state = this.state;
+        state.order.quantity = quantity;
+        this.setState(state);
+    }
 
-	onPlaceOrder() {
-		var state = this.state;
-		state.orderPlaced = true;
-		this.setState(state);
-	}
+    onPlaceOrder() {
+        var state = this.state;
+        state.orderPlaced = true;
+        this.setState(state);
+    }
 
-	onOrderAgain() {
-		this.setState(this.initialState());
-	}
+    onOrderAgain() {
+        this.setState(this.initialState());
+    }
 
-}
-OrderForm.defaultProps = { 
-	pizzaOptions: pizzaOptions,
-	deliveryOptions: deliveryOptions
 }
 
 export default OrderForm;
